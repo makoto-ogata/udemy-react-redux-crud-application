701c4c22f10ee92cb4a50a764f7ca39b2a9078a8
Makoto Ogata <ogatamakoto1976@gmail.com>
2019-12-19 22:37:54 +0900

    implement events new component

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
-import  { READ_EVENS } from '../actions';
+import  { READ_EVENTS } from '../actions';
 
 export default (events = {}, action) => {
   switch (action.type) {
-    case READ_EVENS:
+    case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
     default:
       return events
